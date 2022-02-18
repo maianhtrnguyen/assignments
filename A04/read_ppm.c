4a40e671cffafe8c01b8ec348e7f3462025378e8
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-02-18 10:28:23 -0500

    start HW4

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -3,11 +3,31 @@
 #include <string.h>
 #include "read_ppm.h"
 
+struct ppm_pixel {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
+
 // TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE *infile = NULL;
+  infile = fopen(filename, "r"); // open the file
+
+  if (infile == NULL) {
+    // Print an error if the program cannot open the file
+    printf("Cannot open file: %s\n", argv[1]);
+    exit(1);
+  }
+
+
 
   return NULL;
+}
+
+int main() {
+
 }
 
