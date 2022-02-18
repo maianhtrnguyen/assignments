71fce3664d8b1d09462a420e218950bbc1806ac8
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-02-18 18:43:39 -0500

    finish part 1

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -3,31 +3,54 @@
 #include <string.h>
 #include "read_ppm.h"
 
-struct ppm_pixel {
-    unsigned char red;
-    unsigned char green;
-    unsigned char blue;
-};
-
 // TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
   FILE *infile = NULL;
+  char str[512];
+  unsigned char r; 
+  unsigned char g; 
+  unsigned char b; 
+
   infile = fopen(filename, "r"); // open the file
 
   if (infile == NULL) {
     // Print an error if the program cannot open the file
-    printf("Cannot open file: %s\n", argv[1]);
-    exit(1);
+    printf("Cannot open file: %s\n", filename);
+    return NULL;
   }
 
+  fgets(str, 512, infile); // skip line with ASCII type
+  fgets(str, 512, infile); 
 
+  if (str[0] == '#' || str[0] == ' ') {
+    fgets(str, 512, infile); // skip comment line or blank line
+  }
 
-  return NULL;
+  *w = atoi(&str[0]);
+  *h = atoi(&str[2]);
+  fgets(str, 512, infile);
+
+  struct ppm_pixel *two_d_array;
+  two_d_array = malloc(sizeof(int) * (*w) * (*h));
+
+  if (two_d_array == NULL) {
+    printf("ERROR: malloc failed!\n");
+    return NULL;
+  }
+
+  for (int i = 0; i < (*w * *h); i++) {
+      fscanf(infile, " %hhu %hhu %hhu", &r, &g, &b);
+      two_d_array[i].red = r;
+      two_d_array[i].green = g;
+      two_d_array[i].blue = b;
+  }
+
+  fclose(infile);
+  return two_d_array;
 }
 
-int main() {
 
-}
+
 
--- A04/test_ppm.c
+++ A04/test_ppm.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 
 int main() {
 
   // todo: call read_ppm
+  int height, width = 0;
+  struct ppm_pixel* two_d_array = read_ppm("feep-ascii.ppm", &width, &height);
 
+  printf("Testing file feep-ascii.ppm: %d %d\n", width, height);
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      printf("(%hhu %hhu %hhu) ", two_d_array[i * width + j].red, two_d_array[i * width + j].green, 
+      two_d_array[i * width + j].blue);
+    }
+    printf("\n");
+  } 
+
+  free(two_d_array);
+  two_d_array = NULL;
   return 0;
 }
 
