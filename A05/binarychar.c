39afbda1f7a153da810b7270cc8ad66fa26f6814
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-02-25 10:25:16 -0500

    done part 2

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  char input[128];
+  int magnitude = 0;
+  printf("Enter 8 bits: ");
+  scanf(" %s", input);
+
+  for (int i = strlen(input) - 1; i >= 0; i--) {
+    if (input[i] == '1') {
+      int exp = strlen(input) - i - 1;
+      magnitude += 1 << exp;
+    }
+  }
+
+  printf("Your character is: %c\n", (char) magnitude);
   return 0;
 }
+
