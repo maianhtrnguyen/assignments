af83fb0000aae32f34bb7c20e10266537240f8dd
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-02-25 21:57:33 -0500

    done part 3

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -7,13 +7,63 @@
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  // initialize variables
+  FILE *infile = NULL;
+  char str[512];
+  struct ppm_pixel *two_d_array;
 
-  return NULL;
+  infile = fopen(filename, "rb"); // open the file
+
+  if (infile == NULL) {
+    // Print an error if the program cannot open the file
+    printf("Cannot open file: %s\n", filename);
+    return NULL;
+  }
+
+  fgets(str, 512, infile); // skip line with ASCII type
+  fgets(str, 512, infile); 
+
+  if (str[0] == '#' || str[0] == ' ') {
+    fgets(str, 512, infile); // skip comment line or blank line
+  }
+
+  // get width and height
+  *w = atoi(&str[0]);
+  *h = atoi(&str[2]);
+  fgets(str, 512, infile); // skip line with Maximum color value
+
+  // create 2D array
+  two_d_array = malloc(sizeof(int) * (*w) * (*h));
+
+  if (two_d_array == NULL) {
+    printf("ERROR: malloc failed!\n");
+    return NULL;
+  }
+
+  fread(two_d_array, sizeof(struct ppm_pixel), (*w) * (*h), infile);
+  fclose(infile);
+  return two_d_array;
 }
 
 // TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 extern void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+  FILE *infile = fopen(filename, "wb"); // open the file
 
+  if (infile == NULL) {
+    printf("Cannot open file: %s\n", filename);
+    return;
+  }
+
+  fprintf(infile, "P6 \n%d %d\n255\n", w, h);
+
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      int index = i * w + j;
+      fwrite(pxs[index].colors, 1, 3, infile);
+    }
+  }
+
+  fclose(infile);
 }
--- A05/test_ppm.c
+++ A05/test_ppm.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 
-int main(int argc, char** argv) {
+int main() {
 
+  // todo: call read_ppm
+  int height, width = 0;
+  struct ppm_pixel* two_d_array = read_ppm("feep-raw.ppm", &width, &height);
+
+  if (two_d_array == NULL) {
+    printf("Memory cannot be allocated for the image or the filename is invalid.");
+    exit(1);
+  }
+
+  printf("Testing file feep-raw.ppm: %d %d\n", width, height);
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      int index = i * width + j;
+      printf("(%hhu, %hhu, %hhu) ", two_d_array[index].red, two_d_array[index].green, two_d_array[index].blue);
+    }
+    printf("\n");
+  } 
+
+  free(two_d_array);
+  two_d_array = NULL;
   return 0;
 }
 
