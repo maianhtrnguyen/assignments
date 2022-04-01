3ffd53ff64f71be35a4c20a8b009da788628cb2a
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-04-01 11:54:17 -0400

    working on fragstats

--- A08/mylloc_list.c
+++ A08/mylloc_list.c
@@ -25,6 +25,11 @@ void *malloc (size_t size) {
 }
 
 void free(void *memory) {
+  if (memory != NULL) {
+    struct chunk *current_chunk = (struct chunk*) ((struct chunk*) memory − 1); 
+    current_chunk−>next = flist;
+    flist = current_chunk;
+  } 
   return;
 }
 
@@ -35,17 +40,45 @@ void fragstats(void* buffers[], int len) {
   int smallest_unused_memory = 0;
   float average_unused_memory = 0.0;
   int total_unused_memory = 0;
-  int largest_size = 0;
-  int smallest_size = 0;
-  float average_size = 0.0;
+  int largest_size_free_chunk = 0;
+  int smallest_size_free_chunk = 0;
+  float average_size_free_chunk = 0.0;
 
+  // buffers has the memory currently in use and flist contains the memory not in use
   for (int i = 0; i < len; i++) {
     if (buffer[i] != NULL) {
-      struct chunk* current_chunk = (struct chunk*) buffer[i];
-      total_free_chunks += 1
-      
+      struct chunk *current_chunk = (struct chunk*) ((struct chunk*) buffer[i] − 1); 
+      in_used_chunks += 1;
+      int unused_memory = current_chunk->size - current_chunk->used_size;
+      total_unused_memory += unused_memory;
+
+      if (unused_memory < smallest_unused_memory || smallest_unused_memory == 0) {
+        smallest_unused_memory = unused_memory;
+      } else if (unusued_memory > largest_unused_memory) {
+        largest_unused_memory = unusued_memory;
+      }
     }
   }
+
+  average_unused_memory = total_unused_memory/in_used_chunks;
+
+  struct chunk *current_chunk = flist;
+
+  while (current_chunk != NULL) {
+    total_free_chunks += 1;
+
+    if (current_chunk->size < smallest_size_free_chunk || smallest_size_free_chunk == 0) {
+      smallest_size_free_chunk = current_chunk->size;
+    } else if (current_chunk->size > argest_size_free_chunk ) {
+      argest_size_free_chunk  = current_chunk->size;
+    }
+  }
+
+
+
+
+
+
 
 }
 
