d4121266d0dc0ee2ad892e4f1186f1ad27dc03c5
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-04-01 10:24:18 -0400

    finished reallocate

--- A08/mylloc_list.c
+++ A08/mylloc_list.c
@@ -4,8 +4,10 @@
 
 struct chunk {
   int size;
+  int used_size;
   struct chunk *next;
 };
+
 struct chunk *flist = NULL;
 
 void *malloc (size_t size) {
@@ -27,5 +29,23 @@ void free(void *memory) {
 }
 
 void fragstats(void* buffers[], int len) {
+  int total_free_chunks = 0;
+  int in_used_chunks = 0;
+  int largest_unused_memory = 0;
+  int smallest_unused_memory = 0;
+  float average_unused_memory = 0.0;
+  int total_unused_memory = 0;
+  int largest_size = 0;
+  int smallest_size = 0;
+  float average_size = 0.0;
+
+  for (int i = 0; i < len; i++) {
+    if (buffer[i] != NULL) {
+      struct chunk* current_chunk = (struct chunk*) buffer[i];
+      total_free_chunks += 1
+      
+    }
+  }
+
 }
 
--- A08/reallocate.c
+++ A08/reallocate.c
@@ -5,6 +5,29 @@
 #include "rand.h"
 
 int main (int argc, char* argv[]) {
+  void* initial_top = sbrk(0);
+  printf("The initial top of the heap is %p\n", initial_top);
+  void* memory;
+  
+  for (int i=0; i < 10; i++) {
+    memory = malloc(100);
+
+    if (memory == NULL) {
+      printf("ERROR: malloc failed!\n");
+      exit(1);
+    }
+
+    free(memory);
+    memory = NULL;
+  }
+
+  void* final_top = sbrk(0);
+  printf("The final top of the heap is %p\n", final_top);
+  int difference = (int) (final_top - initial_top);
+  printf("Increased by %d (0x%0x) bytes\n", difference, difference);
 
   return 0 ;
 }
+
+
+
