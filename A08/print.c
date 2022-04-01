362ac86616da12e137bbf08ab5bfdcd9a0ef73e1
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-03-31 20:22:35 -0400

    finished simple

--- A08/print.c
+++ A08/print.c
@@ -5,6 +5,11 @@
 #include "rand.h"
 
 int main (int argc, char* argv[]) {
-
-  return 0 ;
+    void* initial_top = sbrk(0);
+    printf("The initial top of the heap is %p\n", initial_top);
+    void* current_top = sbrk(0);
+    printf("The current top of the heap is %p\n", current_top);
+    int difference = (int) (current_top - initial_top);
+    printf("Increased by %d (0x%0x) bytes\n", difference, difference);
+    return 0 ;
 }
