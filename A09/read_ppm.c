562c8d61977190a48cf9f918af9018a6b7e78c9d
Mai Anh T Nguyen <mnguyen1@goldengate.cs.brynmawr.edu>
2022-04-07 19:24:11 -0400

    finished single_mandelbrot

--- A09/read_ppm.c
+++ A09/read_ppm.c
@@ -3,14 +3,76 @@
 #include <string.h>
 #include "read_ppm.h"
 
+// TODO: Implement this function
+// Feel free to change the function signature if you prefer to implement an 
+// array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+    // initialize variables
+  FILE *infile = NULL;
+  char str[512];
+  struct ppm_pixel *two_d_array;
 
-  // your code here
-  return NULL;
+  infile = fopen(filename, "rb"); // open the file
+
+  if (infile == NULL) {
+    // Print an error if the program cannot open the file
+    printf("Cannot open file: %s\n", filename);
+    return NULL;
+  }
+
+  fgets(str, 512, infile); // skip line with ASCII type
+  fgets(str, 512, infile); 
+
+  if (str[0] == '#' || str[0] == ' ') {
+    fgets(str, 512, infile); // skip comment line or blank line
+  }
+
+  // get width and height
+  *w = atoi(&str[0]);
+  *h = 0;
+
+  // because width and height is separated by a blank space, we have to find the index of the height
+  for (int i = 0; i < strlen(str); i++) {
+    if (str[i] == ' ') {
+      *h = atoi(&str[i + 1]);
+      break;
+    }
+  }
+  
+  fgets(str, 512, infile); // skip line with Maximum color value
+
+  // create 2D array
+  two_d_array = malloc(sizeof(int) * (*w) * (*h));
+
+  if (two_d_array == NULL) {
+    printf("ERROR: malloc failed!\n");
+    return NULL;
+  }
+
+  fread(two_d_array, sizeof(struct ppm_pixel), (*w) * (*h), infile);
+  fclose(infile);
+  return two_d_array;
 }
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+// TODO: Implement this function
+// Feel free to change the function signature if you prefer to implement an 
+// array of arrays
+extern void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+  FILE *infile = fopen(filename, "wb"); // open the file
 
-  // your code here
+  if (infile == NULL) {
+    printf("Cannot open file: %s\n", filename);
+    return;
+  }
+
+  fprintf(infile, "P6 \n%d %d\n255\n", w, h);
+
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      int index = i * w + j;
+      fwrite(pxs[index].colors, 1, 3, infile); // write to your file
+    }
+  }
+
+  fclose(infile);
 }
-
--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -6,7 +6,7 @@
 #include "read_ppm.h"
 
 int main(int argc, char* argv[]) {
-  int size = 480;
+  int size = 2000;
   float xmin = -2.0;
   float xmax = 0.47;
   float ymin = -1.12;
@@ -31,7 +31,74 @@ int main(int argc, char* argv[]) {
   // todo: your work here
   // generate pallet
   srand(time(0));
+  double timer;
+  struct timeval tstart, tend;
+  gettimeofday(&tstart, NULL);
+  struct ppm_pixel* two_d_array = malloc(sizeof(struct ppm_pixel) * size * size);
+  struct ppm_pixel* palette = malloc(sizeof(struct ppm_pixel) * maxIterations);
+  char output_filename[128];
+
+  if (two_d_array == NULL) {
+    printf("ERROR: malloc failed!\n");
+    exit(1);
+  }
+
+  if (palette == NULL) {
+    printf("ERROR: malloc failed!\n");
+    exit(1);
+  }
+
+  // generate random colors
+  for (int i = 0; i < maxIterations; i++) {
+    palette[i].red = rand() % 255;
+    palette[i].green = rand() % 255;
+    palette[i].blue = rand() % 255;
+  }
 
   // compute image
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col < size; col++) {
+      float xfrac = row/size;
+      float yfrac = col/size;
+      float x0 = xmin + xfrac * (xmax - xmin);
+      float y0 = ymin + yfrac * (ymax - ymin);
+
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+
+      while (iter < maxIterations && (x * x + y * y < 2 * 2)) {
+        float xtmp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xtmp;
+        iter += 1;
+      }
+
+      int index = (row * size) + col;
+
+      if (iter < maxIterations) {
+        // write color to image at location (row,col)
+        two_d_array[index] = palette[iter];
+      } else {
+        // assign black color 
+        two_d_array[index].red = 0; 
+        two_d_array[index].green = 0;
+        two_d_array[index].blue = 0;
+      }
+    }
+  }
+
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%dx%d) in %.6f seconds\n", size, size, timer);
+  sprintf(output_filename, "mandelbrot-%d-%.ld.ppm", size, time(0));
+  printf("Writing file: %s\n", output_filename);
+  write_ppm(output_filename, two_d_array, size, size);
+  free(two_d_array);
+  free(palette);
+  two_d_array = NULL;
+  palette = NULL;
+  return 0;
 
 }
+
